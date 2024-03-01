f78c85a5a66e424fdf2c333d36eaf11c52453b2f
Carl Pearson <cwpears@sandia.gov>
2024-03-01 14:27:19 -0700

    test-mpi: print a bit more

--- unit_tests/test_mpi.cpp
+++ unit_tests/test_mpi.cpp
@@ -5,9 +5,10 @@
 int main(int argc, char *argv[]) {
   // Initialize MPI before any call to gtest_mpi
   MPI_Init(&argc, &argv);
-  int rank;
+  int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  std::cerr << "Hello from rank " << rank << "\n";
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  std::cerr << "Hello from rank " << rank << "/" << size << "\n";
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
   return 0;
