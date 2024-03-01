161c3bc93f31c58f514950778e7ff9be63a18c04
Carl Pearson <cwpears@sandia.gov>
2024-03-01 14:26:22 -0700

    formatting

--- unit_tests/test_sendrecv.cpp
+++ unit_tests/test_sendrecv.cpp
@@ -2,14 +2,14 @@
 
 #include "KokkosComm.hpp"
 
-
-template <typename T>
-class SendRecv : public testing::Test {
- public:
+template <typename T> class SendRecv : public testing::Test {
+public:
   using Scalar = T;
 };
 
-using ScalarTypes = ::testing::Types<int, int64_t, float, double, Kokkos::complex<float>, Kokkos::complex<double>>;
+using ScalarTypes =
+    ::testing::Types<int, int64_t, float, double, Kokkos::complex<float>,
+                     Kokkos::complex<double>>;
 TYPED_TEST_SUITE(SendRecv, ScalarTypes);
 
 TYPED_TEST(SendRecv, 1D_contig) {
@@ -24,7 +24,8 @@ TYPED_TEST(SendRecv, 1D_contig) {
 
   if (0 == rank) {
     int dst = 1;
-    Kokkos::parallel_for(a.extent(0), KOKKOS_LAMBDA(const int i){ a(i) = i; });
+    Kokkos::parallel_for(
+        a.extent(0), KOKKOS_LAMBDA(const int i) { a(i) = i; });
     KokkosComm::send(Kokkos::DefaultExecutionSpace(), a, dst, 0,
                      MPI_COMM_WORLD);
   } else if (1 == rank) {
@@ -32,17 +33,17 @@ TYPED_TEST(SendRecv, 1D_contig) {
     KokkosComm::recv(Kokkos::DefaultExecutionSpace(), a, src, 0,
                      MPI_COMM_WORLD);
     int errs;
-    Kokkos::parallel_reduce(a.extent(0), KOKKOS_LAMBDA (const int& i, int& lsum) {
-        lsum += a(i) != i;
-    }, errs);
+    Kokkos::parallel_reduce(
+        a.extent(0),
+        KOKKOS_LAMBDA(const int &i, int &lsum) { lsum += a(i) != i; }, errs);
     ASSERT_EQ(errs, 0);
   }
 }
 
-
 TYPED_TEST(SendRecv, 1D_noncontig) {
   // this is C-style layout, i.e. b(0,0) is next to b(0,1)
-  Kokkos::View<typename TestFixture::Scalar **, Kokkos::LayoutRight> b("a", 10, 10);
+  Kokkos::View<typename TestFixture::Scalar **, Kokkos::LayoutRight> b("a", 10,
+                                                                       10);
   auto a = Kokkos::subview(b, 2, Kokkos::ALL); // take column 2 (non-contiguous)
 
   int rank;
@@ -50,7 +51,8 @@ TYPED_TEST(SendRecv, 1D_noncontig) {
 
   if (0 == rank) {
     int dst = 1;
-    Kokkos::parallel_for(a.extent(0), KOKKOS_LAMBDA(const int i){ a(i) = i; });
+    Kokkos::parallel_for(
+        a.extent(0), KOKKOS_LAMBDA(const int i) { a(i) = i; });
     KokkosComm::send(Kokkos::DefaultExecutionSpace(), a, dst, 0,
                      MPI_COMM_WORLD);
   } else if (1 == rank) {
@@ -58,9 +60,9 @@ TYPED_TEST(SendRecv, 1D_noncontig) {
     KokkosComm::recv(Kokkos::DefaultExecutionSpace(), a, src, 0,
                      MPI_COMM_WORLD);
     int errs;
-    Kokkos::parallel_reduce(a.extent(0), KOKKOS_LAMBDA (const int& i, int& lsum) {
-        lsum += a(i) != i;
-    }, errs);
+    Kokkos::parallel_reduce(
+        a.extent(0),
+        KOKKOS_LAMBDA(const int &i, int &lsum) { lsum += a(i) != i; }, errs);
     ASSERT_EQ(errs, 0);
   }
 }
