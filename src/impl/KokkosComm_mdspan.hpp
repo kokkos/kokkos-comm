f722ad20fd697dfff9d979c96946cdf78c28d0a0
Carl Pearson <cwpears@sandia.gov>
2024-03-11 10:45:32 -0600

    Relicense and Transfer Copyright to NTESS

--- src/impl/KokkosComm_mdspan.hpp
+++ src/impl/KokkosComm_mdspan.hpp
@@ -1,16 +1,18 @@
-// Copyright 2023 Carl Pearson
+//@HEADER
+// ************************************************************************
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
 
 #pragma once
 
