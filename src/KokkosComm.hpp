c62e851df5f1c4c49476a6e104743ba71c02ac00
Cedric Chevalier <cedric.chevalier@cea.fr>
2024-04-09 15:41:04 +0200

    KokkosComm_configuration.hpp
    Set KokkosComm definitions in this generated header file
    
    KokkosComm Version informations are also located there.

--- src/KokkosComm.hpp
+++ src/KokkosComm.hpp
@@ -16,8 +16,9 @@
 
 #pragma once
 
+#include "KokkosComm_configuration.hpp"
+
 #include "KokkosComm_collective.hpp"
-#include "KokkosComm_version.hpp"
 #include "KokkosComm_isend.hpp"
 #include "KokkosComm_irecv.hpp"
 #include "KokkosComm_recv.hpp"
