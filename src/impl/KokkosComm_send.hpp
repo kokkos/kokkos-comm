f8ab24491c68f8fc556190ab74d42789245336d2
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2024-04-11 14:03:28 +0200

    Add Ready and Synchronous mode sends

--- src/impl/KokkosComm_send.hpp
+++ src/impl/KokkosComm_send.hpp
@@ -18,12 +18,14 @@
 
 #include <Kokkos_Core.hpp>
 
+#include "KokkosComm_mode.hpp"
 #include "KokkosComm_pack_traits.hpp"
 
 // impl
 #include "KokkosComm_include_mpi.hpp"
 
 namespace KokkosComm::Impl {
+
 template <KokkosExecutionSpace ExecSpace, KokkosView SendView>
 void send(const ExecSpace &space, const SendView &sv, int dest, int tag,
           MPI_Comm comm) {
@@ -42,4 +44,43 @@ void send(const ExecSpace &space, const SendView &sv, int dest, int tag,
 
   Kokkos::Tools::popRegion();
 }
+
+template <KokkosExecutionSpace ExecSpace, KokkosView SendView>
+void rsend(const ExecSpace &space, const SendView &sv, int dest, int tag,
+          MPI_Comm comm) {
+  Kokkos::Tools::pushRegion("KokkosComm::Impl::rsend");
+
+  using Packer = typename KokkosComm::PackTraits<SendView>::packer_type;
+
+  if (KokkosComm::PackTraits<SendView>::needs_pack(sv)) {
+    auto args = Packer::pack(space, sv);
+    space.fence();
+    MPI_Rsend(args.view.data(), args.count, args.datatype, dest, tag, comm);
+  } else {
+    using SendScalar = typename SendView::value_type;
+    MPI_Rsend(sv.data(), sv.span(), mpi_type_v<SendScalar>, dest, tag, comm);
+  }
+
+  Kokkos::Tools::popRegion();
+}
+
+template <KokkosExecutionSpace ExecSpace, KokkosView SendView>
+void ssend(const ExecSpace &space, const SendView &sv, int dest, int tag,
+          MPI_Comm comm) {
+  Kokkos::Tools::pushRegion("KokkosComm::Impl::ssend");
+
+  using Packer = typename KokkosComm::PackTraits<SendView>::packer_type;
+
+  if (KokkosComm::PackTraits<SendView>::needs_pack(sv)) {
+    auto args = Packer::pack(space, sv);
+    space.fence();
+    MPI_Ssend(args.view.data(), args.count, args.datatype, dest, tag, comm);
+  } else {
+    using SendScalar = typename SendView::value_type;
+    MPI_Ssend(sv.data(), sv.span(), mpi_type_v<SendScalar>, dest, tag, comm);
+  }
+
+  Kokkos::Tools::popRegion();
+}
+
 }  // namespace KokkosComm::Impl
