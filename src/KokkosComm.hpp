d623552f0b05a2e44ca817167d294877afc25af9
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2024-04-17 16:46:00 +0200

    Move `CommMode` back to its own file to avoid circular header deps

--- src/KokkosComm.hpp
+++ src/KokkosComm.hpp
@@ -22,28 +22,12 @@
 #include "KokkosComm_recv.hpp"
 #include "KokkosComm_send.hpp"
 #include "KokkosComm_concepts.hpp"
+#include "KokkosComm_comm_mode.hpp"
 
 #include <Kokkos_Core.hpp>
 
 namespace KokkosComm {
 
-// Scoped enumeration to specify the communication mode of a sending operation.
-// See section 3.4 of the MPI standard for a complete specification.
-enum class CommMode {
-  // Standard mode: MPI implementation decides whether outgoing messages will
-  // be buffered. Send operations can be started whether or not a matching
-  // receive has been started. They may complete before a matching receive is
-  // started. Standard mode is non-local: successful completion of the send
-  // operation may depend on the occurrence of a matching receive.
-  Standard,
-  // Ready mode: Send operations may be started only if the matching receive is
-  // already started.
-  Ready,
-  // Synchronous mode: Send operations complete successfully only if a matching
-  // receive is started, and the receive operation has started to receive the
-  // message sent.
-  Synchronous,
-};
 
 template <CommMode SendMode = CommMode::Standard,
           KokkosExecutionSpace ExecSpace, KokkosView SendView>
