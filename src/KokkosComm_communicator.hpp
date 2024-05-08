f0faee8bd10c5e9563c71ef842a5ecb084e5a13b
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2024-05-08 20:20:41 +0200

    size and rank getters

--- src/KokkosComm_communicator.hpp
+++ src/KokkosComm_communicator.hpp
@@ -8,4 +8,18 @@ private:
 public;
   Communicator(MPI_Comm mpi_communicator): _raw_comm{ mpi_communicator } {}
   operator MPI_Comm(){ return _raw_comm; }
+
+  int size() const
+  {
+    int size;
+    MPI_Comm_size( _raw_comm, &size );
+    return size;
+  }
+
+  int rank() const
+  {
+    int rank;
+    MPI_Comm_rank( _raw_comm, &rank );
+    return rank;
+  }
 };
