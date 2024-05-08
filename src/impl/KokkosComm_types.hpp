e168141d05e2e1f98894ab5bf1f374a05b24b35f
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2024-05-08 19:50:16 +0200

    Update KokkosComm_types.hpp

--- src/impl/KokkosComm_types.hpp
+++ src/impl/KokkosComm_types.hpp
@@ -28,12 +28,8 @@ MPI_Datatype mpi_type() {
 };
 
 template <>
-inline MPI_Datatype mpi_type<unsigned int>() {
-  return MPI_UNSIGNED;
-}
-template <>
-inline MPI_Datatype mpi_type<unsigned long>() {
-  return MPI_UNSIGNED_LONG;
+inline MPI_Datatype mpi_type<int>() {
+  return MPI_INT;
 }
 template <>
 inline MPI_Datatype mpi_type<long int>() {
@@ -43,18 +39,33 @@ template <>
 inline MPI_Datatype mpi_type<long long>() {
   return MPI_LONG_LONG;
 }
+
 template <>
-inline MPI_Datatype mpi_type<int>() {
-  return MPI_INT;
+inline MPI_Datatype mpi_type<unsigned int>() {
+  return MPI_UNSIGNED;
+}
+template <>
+inline MPI_Datatype mpi_type<unsigned long>() {
+  return MPI_UNSIGNED_LONG;
+}
+template <>
+inline MPI_Datatype mpi_type<unsigned long>() {
+  return MPI_UNSIGNED_LONG_LONG;
+}
+
+template <>
+inline MPI_Datatype mpi_type<float>() {
+  return MPI_FLOAT;
 }
 template <>
 inline MPI_Datatype mpi_type<double>() {
   return MPI_DOUBLE;
 }
 template <>
-inline MPI_Datatype mpi_type<float>() {
-  return MPI_FLOAT;
+inline MPI_Datatype mpi_type<long double>() {
+  return MPI_LONG_DOUBLE;
 }
+
 template <>
 inline MPI_Datatype mpi_type<Kokkos::complex<float>>() {
   return MPI_COMPLEX;
@@ -62,6 +73,55 @@ inline MPI_Datatype mpi_type<Kokkos::complex<float>>() {
 template <>
 inline MPI_Datatype mpi_type<Kokkos::complex<double>>() {
   return MPI_DOUBLE_COMPLEX;
+}
+
+template <>
+inline MPI_Datatype mpi_type<int8_t>() {
+  return MPI_INT8_T;
+}
+template <>
+inline MPI_Datatype mpi_type<int16_t>() {
+  return MPI_INT16_T;
+}
+template <>
+inline MPI_Datatype mpi_type<int32_t>() {
+  return MPI_INT32_T;
+}
+template <>
+inline MPI_Datatype mpi_type<int64_t>() {
+  return MPI_INT64_T;
+}
+
+template <>
+inline MPI_Datatype mpi_type<uint8_t>() {
+  return MPI_UINT8_T;
+}
+template <>
+inline MPI_Datatype mpi_type<uint16_t>() {
+  return MPI_UINT16_T;
+}
+template <>
+inline MPI_Datatype mpi_type<uint32_t>() {
+  return MPI_UINT32_T;
+}
+template <>
+inline MPI_Datatype mpi_type<uint64_t>() {
+  return MPI_UINT64_T;
+}
+
+template <>
+inline MPI_Datatype mpi_type<size_t>() {
+  if constexpr ( sizeof(size_t) == 1 ) return MPI_UINT8_T;
+	if constexpr ( sizeof(size_t) == 2 ) return MPI_UINT16_T;
+	if constexpr ( sizeof(size_t) == 4 ) return MPI_UINT32_T;
+	if constexpr ( sizeof(size_t) == 8 ) return MPI_UINT64_T;
+}
+template <>
+inline MPI_Datatype mpi_type<ssize_t>() {
+  if constexpr ( sizeof(ssize_t) == 1 ) return MPI_INT8_T;
+	if constexpr ( sizeof(ssize_t) == 2 ) return MPI_INT16_T;
+	if constexpr ( sizeof(ssize_t) == 4 ) return MPI_INT32_T;
+	if constexpr ( sizeof(ssize_t) == 8 ) return MPI_INT64_T;
 }
 
 template <typename Scalar>
