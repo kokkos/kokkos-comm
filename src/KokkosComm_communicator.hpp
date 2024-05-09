6e38f78f50ae186e5fd4e91dbb37d886be74a3e8
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2024-05-09 16:56:26 +0200

    async scheme

--- src/KokkosComm_communicator.hpp
+++ src/KokkosComm_communicator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <Kokkos_Core.hpp>
 #include "KokkosComm_concepts.hpp"
 
@@ -63,17 +64,6 @@ namespace KokkosComm {
     }
 
     template< typename T, class... ARGS >
-    void recv( Kokkos::View< T*, ARGS... > recv_view, int src_rank, int tag = 0 ) const {
-      MPI_Recv( recv_view.data(), recv_view.size(), mpi_type<T>(), src_rank, tag, _raw_comm, MPI_STATUS_IGNORE );
-    }
-
-    template< typename T, class... ARGS >
-    void sendrecv( Kokkos::View< T const*, ARGS... > send_view, Kokkos::View< T*, ARGS... > recv_view, int rank, int tag = 0 ) const {
-      MPI_Sendrecv( send_view.data(), send_view.size(), mpi_type<T>(), rank, tag, _raw_comm,
-                    recv_view.data(), recv_view.size(), mpi_type<T>(), rank, tag, _raw_comm, MPI_STATUS_IGNORE );
-    }
-
-    template< typename T, class... ARGS >
     KokkosComm::Request isend( Kokkos::View< T const*, ARGS... > const& send_view, int dest_rank, int tag = 0, CommMode mode = CommMode::Default ){
       MPI_Request req;
       switch ( mode ){
@@ -97,6 +87,19 @@ namespace KokkosComm {
       }
       return KokkosComm::Request{ req };
     }
+
+
+    template< typename T, class... ARGS >
+    void recv( Kokkos::View< T*, ARGS... > recv_view, int src_rank, int tag = 0 ) const {
+      MPI_Recv( recv_view.data(), recv_view.size(), mpi_type<T>(), src_rank, tag, _raw_comm, MPI_STATUS_IGNORE );
+    }
+
+
+    template< typename T, class... ARGS >
+    void sendrecv( Kokkos::View< T const*, ARGS... > send_view, Kokkos::View< T*, ARGS... > recv_view, int rank, int tag = 0 ) const {
+      MPI_Sendrecv( send_view.data(), send_view.size(), mpi_type<T>(), rank, tag, _raw_comm,
+                    recv_view.data(), recv_view.size(), mpi_type<T>(), rank, tag, _raw_comm, MPI_STATUS_IGNORE );
+    }
   };
 
   template< typename T >
@@ -104,5 +107,62 @@ namespace KokkosComm {
     cs = T{ comm };
     cs.launch();
     cs.wait();
+  };
+
+  template< typename T >
+  class BufferedScheme
+  {
+   private:
+    using SendView = Kokkos::View< T const*, Kokkos::LayoutStride, Kokkos::MemoryTraits< Kokkos::Unmanaged > >;
+    using RecvView = Kokkos::View< T*, Kokkos::LayoutStride, Kokkos::MemoryTraits< Kokkos::Unmanaged > >;
+
+    Communicator _comm;
+
+    std::vector< int > _src_ranks;
+    std::vector< RecvView > _recv_views;
+    bool _recv_updated;
+   
+    std::vector< int > _dest_ranks;
+    std::vector< SendView > _send_views;
+    bool _send_updated;
+ 
+    Kokkos::View< T* > _recv_buffer;
+    Kokkos::View< T* > _send_buffer;
+ 
+    std::vector< Request > _recv_requests;
+    std::future< void > _wait_handle;
+
+   public:
+    void launch(){
+      _wait_handle = std::async( std::launch::async, [&]{
+        if ( _recv_updated )
+          Kokkos::resize( _recv_buffer, std::accumulate( ... ) );
+        
+        int offset = 0;
+        for ( auto& [ src, view, req ] : std::ranges::views::zip( _src_ranks, _recv_views, _recv_requests ) ){
+          req = comm.irecv( Kokkos::subview( _recv_buffer, Kokkos::pair{ offset, offset+view.size } ), src );
+          offset += view.size();
+        }
+
+
+        if ( _send_updated )
+          Kokkos::resize( _send_buffer, std::accumulate( ... ) );
+
+        // pack
+
+        offset = 0;
+        for ( auto& [ dest, view ] : std::ranges::views::zip( _dest_ranks, _send_views ) ){
+          comm.isend( Kokkos::subview( _send_buffer, Kokkos::pair{ offset, offset+view.size } ), dest ).free();
+          offset += view.size();
+        }
+
+
+        MPI_Waitall( _recv_requests.size(), _recv_requests.data(), MPI_STATUSES_IGNORE ); 
+        
+        // unpack
+      });
+    }
+
+    void wait(){ _wait_handle.wait(); }
   };
 }
