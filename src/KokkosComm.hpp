5614211cb2f218566d72b426d72e3fea0867be1f
Gabriel Dos Santos <dss.gabriel@protonmail.com>
2024-05-16 00:25:41 +0200

    feat: add `Universe` class to handle KokkosComm initialization/finalization

--- src/KokkosComm.hpp
+++ src/KokkosComm.hpp
@@ -28,6 +28,42 @@
 
 namespace KokkosComm {
 
+class Universe {
+ public:
+  Universe(int &argc, char *argv[]) : Universe(argc, argv, 0) {}
+
+  Universe(int &argc, char *argv[], size_t buf_size) : _buffer(buf_size) {
+    int is_initialized;
+    MPI_Initialized(&is_initialized);
+    if (0 == is_initialized) {
+      int required = MPI_THREAD_MULTIPLE, provided;
+      MPI_Init_thread(&argc, &argv, required, &provided);
+    }
+  }
+
+  ~Universe() {
+    detach_buffer();
+    int is_finalized;
+    MPI_Finalized(&is_finalized);
+    if (0 == is_finalized) {
+      MPI_Finalize();
+    }
+  }
+
+  auto set_buffer_size(size_t size) -> void {
+    detach_buffer();
+    if (0 <= size) {
+      _buffer.resize(size);
+      MPI_Buffer_attach(_buffer.data(), _buffer.size());
+    }
+  }
+
+  auto detach_buffer(void) -> void { MPI_Buffer_detach(&_buffer.data(), &_buffer.size()); }
+
+ private:
+  std::vector<uint8_t> _buffer;
+};
+
 template <CommMode SendMode = CommMode::Default, KokkosExecutionSpace ExecSpace, KokkosView SendView>
 Req isend(const ExecSpace &space, const SendView &sv, int dest, int tag, MPI_Comm comm) {
   return Impl::isend<SendMode>(space, sv, dest, tag, comm);
