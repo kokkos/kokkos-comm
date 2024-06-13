6167b915ff78ab459683fc763bd16ffa618f8f5c
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2024-06-13 23:32:21 +0200

    feat(init/fini): add basic initialization and finalization
    KokkosComm should now be initialized (and finalized) these new
    functions.
    Users now must _not_ initialize/finalize MPI, nor Kokkos by themselves.

--- src/KokkosComm.hpp
+++ src/KokkosComm.hpp
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include "KokkosComm_include_mpi.hpp"
 #include "KokkosComm_collective.hpp"
 #include "KokkosComm_version.hpp"
 #include "KokkosComm_isend.hpp"
@@ -28,7 +29,55 @@
 
 #include <Kokkos_Core.hpp>
 
+#include <algorithm>
+#include <cstdio>
+#include <string_view>
+
 namespace KokkosComm {
+
+inline void initialize(int &argc, char ***argv) {
+  int flag;
+  MPI_Initialized(&flag);
+  // Eagerly abort if MPI has already been initialized
+  if (0 != flag) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (0 == rank) {
+      fprintf(stderr, "error: MPI must not be initialized prior to initializing KokkosComm\n");
+    }
+    MPI_Abort(MPI_COMM_WORLD, -1);
+  }
+
+  int provided;
+  MPI_Init_thread(&argc, argv, MPI_THREAD_MULTIPLE, &provided);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Abort if MPI failed to provide Thread Multiple
+  if (MPI_THREAD_MULTIPLE != provided) {
+    if (0 == rank) {
+      fprintf(stderr, "error: failed to initialized with required thread support\n");
+    }
+    MPI_Abort(MPI_COMM_WORLD, -1);
+  }
+
+  // Strip "--help" and "--kokkos-help" from the flags passed to Kokkos if we are not on rank 0 to prevent Kokkos
+  // from printing the help message multiple times.
+  if (0 != rank) {
+    auto *help_it = std::find_if(*argv, *argv + argc,
+                                 [](std::string_view const &x) { return x == "--help" || x == "--kokkos-help"; });
+    if (help_it != *argv + argc) {
+      std::swap(*help_it, *(*argv + argc - 1));
+      --argc;
+    }
+  }
+  Kokkos::initialize(argc, *argv);
+}
+
+inline void finalize() {
+  Kokkos::finalize();
+  MPI_Finalize();
+}
 
 template <CommMode SendMode = CommMode::Default, KokkosExecutionSpace ExecSpace, KokkosView SendView>
 Req isend(const ExecSpace &space, const SendView &sv, int dest, int tag, MPI_Comm comm) {
