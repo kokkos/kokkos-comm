67c65bc4835b562e6faf938435fdd9f72e07695b
nicoleavans <cnicoleavans@live.com>
2024-06-18 13:58:52 -0600

    tried recommended clang format

--- src/KokkosComm.hpp
+++ src/KokkosComm.hpp
@@ -29,10 +29,10 @@
 #include <Kokkos_Core.hpp>
 
 namespace KokkosComm {
-using Impl::isend;
-using Impl::irecv;
-using Impl::send;
-using Impl::recv;
 using Impl::alltoall;
+using Impl::irecv;
+using Impl::isend;
+using Impl::recv;
+using Impl::send;
 
 }  // namespace KokkosComm
