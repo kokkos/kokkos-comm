e84fda8339472d0369ba145b4f220de1b765bc06
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2024-11-14 19:41:04 +0100

    feat: add NCCL `reduce`

---
+++ src/KokkosComm/nccl/reduce.hpp
@@ -0,0 +1,83 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#pragma once
+
+#include <KokkosComm/concepts.hpp>
+#include <KokkosComm/traits.hpp>
+#include <KokkosComm/nccl/impl/pack_traits.hpp>
+#include <KokkosComm/nccl/impl/types.hpp>
+
+#include <Kokkos_Core.hpp>
+
+namespace KokkosComm::Experimental::nccl {
+
+template <KokkosExecutionSpace ExecSpace, KokkosView SendView, KokkosView RecvView>
+void reduce(const ExecSpace& space, const SendView &sv, const RecvView &rv, ncclOp_t op, int root, ncclComm_t comm) {
+  Kokkos::Tools::pushRegion("KokkosComm::Impl::reduce");
+  using SPT = KokkosComm::PackTraits<SendView>;
+  using RPT = KokkosComm::PackTraits<RecvView>;
+
+  if (SPT::is_contiguous(sv) && RPT::is_contiguous(rv)) {
+  } else {
+    throw std::runtime_error("only contiguous views supported for low-level reduce");
+  }
+  Kokkos::Tools::popRegion();
+}
+
+template <KokkosExecutionSpace ExecSpace, KokkosView SendView, KokkosView RecvView>
+void reduce(const ExecSpace &space, const SendView &sv, const RecvView &rv, ncclOp_t op, int root, ncclComm_t comm) {
+  Kokkos::Tools::pushRegion("KokkosComm::Impl::reduce");
+
+  // TODO: We should refactor this to use our generic `Handle<NCCL>` and retrieve the rank with it
+  const int rank = [=]() -> int {
+    int _r;
+    ncclCommUserRank(comm, &_r);
+    return _r;
+  }();
+
+  using SendPacker = typename KokkosComm::PackTraits<SendView>::packer_type;
+  using RecvPacker = typename KokkosComm::PackTraits<RecvView>::packer_type;
+
+  if (!KokkosComm::is_contiguous(sv)) {
+    auto send_args = SendPacker::pack(space, sv);
+    space.fence();
+    if ((root == rank) && !KokkosComm::is_contiguous(rv)) {
+      auto recv_args = RecvPacker::allocate_packed_for(space, "reduce recv", rv);
+      space.fence();
+      using SendScalar = typename SendView::non_const_value_type;
+      ncclReduce(send_args.view.data(), recv_args.view.data(), send_args.count, send_args.datatype, op, root, comm, space.cuda_stream());
+      RecvPacker::unpack_into(space, rv, recvArgs.view);
+    } else {
+      space.fence(); // is this fence necessary?
+      ncclReduce(send_args.view.data(), rv.data(), send_args.count, send_args.datatype, op, root, comm, space.cuda_stream());
+    }
+  } else {
+    using SendScalar = typename SendView::value_type;
+    if ((root == rank) && !KokkosComm::is_contiguous(rv)) {
+      auto recv_args = RecvPacker::allocate_packed_for(space, "reduce recv", rv);
+      space.fence();
+      ncclReduce(sv.data(), recv_args.view.data(), sv.span(), KokkosComm::Experimental::nccl::Impl::datatype_v<SendScalar>, op, root, comm, space.cuda_stream());
+      RecvPacker::unpack_into(space, rv, recvArgs.view);
+    } else {
+      space.fence(); // is this fence necessary?
+      ncclReduce(sv.data(), rv.data(), sv.span(), KokkosComm::Experimental::nccl::Impl::datatype_v<SendScalar>, op, root, comm, space.cuda_stream());
+    }
+  }
+
+  Kokkos::Tools::popRegion();
+}
+}  // namespace KokkosComm::mpi
