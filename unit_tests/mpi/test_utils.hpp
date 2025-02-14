bff47775074dbd9f1d773cccecda076acd452cb6
Carl Pearson <cwpears@sandia.gov>
2025-02-14 13:26:50 -0700

    add missing test_utils.hpp
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

---
+++ unit_tests/mpi/test_utils.hpp
@@ -0,0 +1,29 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#pragma once
+
+#include <mpi.h>
+
+inline int reduce(int localErrs) {
+  int globalErrs = 0;
+  int rank       = 0;
+  int size       = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Reduce(&localErrs, &globalErrs, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  return globalErrs;
+}
