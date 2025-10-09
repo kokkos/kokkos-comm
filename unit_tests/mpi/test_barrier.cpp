618fd11ce8ccfc179eee385718c468311cffe742
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2025-10-09 15:03:28 +0200

    test(mpi): make `barrier` test MPI-only
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- unit_tests/mpi/test_barrier.cpp
+++ unit_tests/mpi/test_barrier.cpp
@@ -7,6 +7,9 @@
 
 namespace {
 
-TEST(Barrier, 0) { KokkosComm::barrier(KokkosComm::Handle<>{}); }
+TEST(Barrier, 0) {
+  auto h = KokkosComm::Handle<KokkosComm::Mpi>{};
+  KokkosComm::mpi::barrier(h.mpi_comm());
+}
 
 }  // namespace
