bca86037fc00618781eab75520bb8b9b7b731291
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2025-12-01 16:58:40 +0100

    feat(mpi): add non-blocking broadcast
    Prepare for exposure in "core" API.
    
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- src/KokkosComm/mpi/broadcast.hpp
+++ src/KokkosComm/mpi/broadcast.hpp
@@ -8,10 +8,30 @@
 
 #include <KokkosComm/concepts.hpp>
 #include <KokkosComm/traits.hpp>
+#include "mpi_space.hpp"
+#include "req.hpp"
 
 #include "impl/types.hpp"
+#include "impl/error_handling.hpp"
 
 namespace KokkosComm::mpi {
+
+template <KokkosExecutionSpace ExecSpace, KokkosView View>
+auto ibroadcast(const ExecSpace& space, View& v, int root, MPI_Comm comm) -> Req<Mpi> {
+  using T = typename View::non_const_value_type;
+  Kokkos::Tools::pushRegion("KokkosComm::mpi::ibroadcast");
+  fail_if(!is_contiguous(v), "KokkosComm::mpi::ibroadcast: unimplemented for non-contiguous views");
+
+  // Sync: Work in space may have been used to produce view data.
+  space.fence("fence before non-blocking broadcast");
+
+  Req<Mpi> req;
+  MPI_Ibcast(data_handle(v), span(v), Impl::mpi_type_v<T>, root, comm, &req.mpi_request());
+  req.extend_view_lifetime(v);
+
+  Kokkos::Tools::popRegion();
+  return req;
+}
 
 template <KokkosView View>
 void broadcast(View const& v, int root, MPI_Comm comm) {
