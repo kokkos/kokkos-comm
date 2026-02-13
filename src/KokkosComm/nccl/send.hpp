e27869761d63714a81cd212d6222db4109705d9e
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2026-02-13 19:30:12 +0100

    chore(nccl): typo
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- src/KokkosComm/nccl/send.hpp
+++ src/KokkosComm/nccl/send.hpp
@@ -31,7 +31,7 @@ auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm)
     using Packer = typename Impl::PackTraits<SendView>::packer_type;
     auto pckd_sv = Packer::pack(space, "pckd_sv", sv);
     KC_NCCL_CHECK(
-        ncclSend(data_handle(pckd_sv.view_), pcdk_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream()));
+        ncclSend(data_handle(pckd_sv.view_), pckd_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream()));
     req.capture_stream_state(space.cuda_stream());
     req.extend_view_lifetime(pckd_sv.view_);
   }
