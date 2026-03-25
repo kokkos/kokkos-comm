252e8db928aa08a374544e74a88b8351b5726fd0
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2026-03-25 15:26:33 +0100

    feat(core): refactor contiguous packing to support any dims
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- src/KokkosComm/impl/contiguous.hpp
+++ src/KokkosComm/impl/contiguous.hpp
@@ -3,7 +3,9 @@
 
 #pragma once
 
+#include <cstddef>
 #include <string>
+#include <utility>
 
 #include <Kokkos_Core.hpp>
 
@@ -21,31 +23,23 @@ struct contiguous_view {
 template <KokkosView View>
 using contiguous_view_t = contiguous_view<View>::type;
 
-template <KokkosView View, KokkosExecutionSpace Space>
-auto allocate_contiguous_for(const Space &space, const std::string &label, View &v) {
-  using non_const_packed_view_type = contiguous_view_t<View>;
-
-  if constexpr (KokkosComm::rank<View>() == 1) {
-    return non_const_packed_view_type(Kokkos::view_alloc(space, Kokkos::WithoutInitializing, label), v.extent(0));
-  } else if constexpr (KokkosComm::rank<View>() == 2) {
-    return non_const_packed_view_type(Kokkos::view_alloc(space, Kokkos::WithoutInitializing, label), v.extent(0),
-                                      v.extent(1));
-  } else {
-    static_assert(std::is_void_v<View>, "allocate_contiguous_for for views > rank 2 not implemented");
+template <KokkosExecutionSpace Exec, KokkosView View>
+auto allocate_contiguous_for(const Exec& exec, const View& v, const std::string& label = "contiguous_view") {
+  using ContigView = contiguous_view_t<View>;
+  return [&]<size_t... Is>(std::index_sequence<Is...>) {
+    return ContigView(Kokkos::view_alloc(exec, Kokkos::WithoutInitializing, label), v.extent(Is)...);
   }
+  (std::make_index_sequence<rank<View>()>{});
 }
 
-template <KokkosExecutionSpace Space, KokkosView DstView, KokkosView SrcView>
-auto resize_contiguous_for(const Space &space, DstView &out, const SrcView &in) {
-  static_assert(DstView::rank == SrcView::rank, "");
-
-  if constexpr (KokkosComm::rank<DstView>() == 1) {
-    Kokkos::realloc(Kokkos::view_alloc(space, Kokkos::WithoutInitializing), out, in.extent(0));
-  } else if constexpr (KokkosComm::rank<DstView>() == 2) {
-    Kokkos::realloc(Kokkos::view_alloc(space, Kokkos::WithoutInitializing), out, in.extent(0), in.extent(1));
-  } else {
-    static_assert(std::is_void_v<DstView>, "realloc_contiguous_for for views > rank 2 not implemented");
+template <KokkosExecutionSpace Exec, KokkosView DstV, KokkosView SrcV>
+auto resize_contiguous_for(const Exec& exec, const DstV& dst, const SrcV& src) {
+  static_assert(rank<DstV>() == rank<SrcV>(),
+                "KokkosComm::Impl::resize_contiguous_for: source and destination Views must have the same rank");
+  [&]<size_t... Is>(std::index_sequence<Is...>) {
+    Kokkos::realloc(Kokkos::view_alloc(exec, Kokkos::WithoutInitializing), dst, src.extent(Is)...);
   }
+  (std::make_index_sequence<rank<DstV>()>{});
 }
 
 }  // namespace KokkosComm::Impl
