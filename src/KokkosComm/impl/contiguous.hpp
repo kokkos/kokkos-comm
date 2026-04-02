5f242015aa5235a16250e51fb1b8c79f54fb2a6e
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2026-04-02 21:05:02 +0200

    refactor(core): remove `resize_contiguous_for` API
    Removed because unused in the Kokkos Comm code base.
    Additionally, it was wrongly implemented. According to the latest Kokkos
    docs (https://kokkos.org/kokkos-core-wiki/API/core/view/realloc.html),
    there is no overload of `realloc` which allows to pass a `view_alloc`
    result (probably this was an artifact from copy-pasting the code of
    `allocate_contiguous_for`?), a destination, and the reshaped extents.

--- src/KokkosComm/impl/contiguous.hpp
+++ src/KokkosComm/impl/contiguous.hpp
@@ -16,8 +16,10 @@ namespace KokkosComm::Impl {
 
 template <KokkosView View>
 struct contiguous_view {
-  using type = Kokkos::View<typename View::non_const_data_type, typename View::execution_space::array_layout,
-                            typename View::memory_space>;
+  using type = Kokkos::View<
+      typename View::non_const_data_type,
+      typename View::execution_space::array_layout,
+      typename View::memory_space>;
 };
 
 template <KokkosView View>
@@ -32,28 +34,9 @@ using contiguous_view_t = contiguous_view<View>::type;
 template <KokkosExecutionSpace Exec, KokkosView View>
 auto allocate_contiguous_for(const Exec& exec, const std::string& label, const View& v) -> contiguous_view_t<View> {
   // Unpack `v` extents into the `ContigView` constructor
-  return [&label, &exec, &v ]<size_t... Is>(std::index_sequence<Is...>) {
+  return [&label, &exec, &v]<size_t... Is>(std::index_sequence<Is...>) {
     return contiguous_view_t<View>(Kokkos::view_alloc(exec, Kokkos::WithoutInitializing, label), v.extent(Is)...);
-  }
-  (std::make_index_sequence<rank<View>()>{});
-}
-
-/// @brief Resize a View into a contiguous one suitable for packing a non-contiguous View.
-/// @tparam Exec A Kokkos Execution Space type.
-/// @tparam DstV The Kokkos View type of the destination View to resize.
-/// @tparam DstV The Kokkos View type of the source View to resize for.
-/// @param exec The execution space instance in which to perform the view reallocation.
-/// @param dst The View to resize.
-/// @param src The View to make a suitable contiguous resize for.
-template <KokkosExecutionSpace Exec, KokkosView DstV, KokkosView SrcV>
-auto resize_contiguous_for(const Exec& exec, const DstV& dst, const SrcV& src) -> void {
-  static_assert(rank<DstV>() == rank<SrcV>(),
-                "KokkosComm::Impl::resize_contiguous_for: source and destination Views must have the same rank");
-  // Unpack `src` extents into `realloc` call
-  [&exec, &dst, &src ]<size_t... Is>(std::index_sequence<Is...>) {
-    Kokkos::realloc(Kokkos::view_alloc(exec, Kokkos::WithoutInitializing), dst, src.extent(Is)...);
-  }
-  (std::make_index_sequence<rank<DstV>()>{});
+  }(std::make_index_sequence<rank<View>()>{});
 }
 
 }  // namespace KokkosComm::Impl
