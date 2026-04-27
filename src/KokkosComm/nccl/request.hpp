499591a1331c01dad2c9753204d0b8ac095a7ff2
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2026-04-27 15:55:20 +0200

    fix(nccl): actually return the result of req `test` in free func
    Signed-off-by: Gabriel Dos Santos <gabriel.dossantos@cea.fr>

--- src/KokkosComm/nccl/request.hpp
+++ src/KokkosComm/nccl/request.hpp
@@ -194,6 +194,6 @@ inline auto wait_any(std::span<Request<Experimental::NcclSpace>> requests)
 
 /// @brief Queries the request for completion of the associated operation.
 /// @param request A reference on the request to query its completion.
-inline auto test(Request<Experimental::NcclSpace>& request) -> bool { request.test(); }
+inline auto test(Request<Experimental::NcclSpace>& request) -> bool { return request.test(); }
 
 }  // namespace KokkosComm
