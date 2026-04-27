kokkos/kokkos-comm

commit acb1fb2b8c0d43e04b3d96da3156a41f2f969414

refactor: rename and rework `Request`s impls (#216)
* refactor: rename `Req` to `Request`

* refactor(mpi): rework implementation of MPI requests

- Removed dead code (`ViewHolderBase`)
- Eliminated internal `Record` struct
  - Requests are now move-only:
    - Copy ctor/assign op are deleted
    - Wrapping in a `shared_ptr` is not needed anymore
    - Match the semantics of `MPI_Request` objects and how wrappers
      around them are implemented in other libs (e.g. kaMPIng)
- Added new member functions:
  - `request` (const ref)
  - `request_ptr` (both const and non-const ptr)
  - `wait` (carries impl)
  - `test` (carries impl)
  - Related friend functions call these directly
- Renamed callback registration:
  = ~~`call_after_mpi_wait`~~ -> `add_callback`
- Fixed `wait_all` and `wait_any`:
  - Construct a temporary container for holding `MPI_Request` objects
    and directly calling `MPI_Wait{all,any}`
- Improved error handling
- Added support for querying the request with `test`

* refactor(nccl): rework implementation of NCCL

- Removed dead code (`ViewHolderBase`)
- Changed internal request handle:
  - ~~`cudaStream_t`~~ -> `cudaEvent_t`
  - Create an event from a stream capture
  - Allows for synchronizing only up-to the recorded event,
    not the full stream (which avoids potentially waiting
    on work submitted _after_ the request was enqueued)
- Eliminated internal `Record` struct
  - Requests are now move-only:
    - Copy ctor/assign op are deleted
    - Wrapping in a `shared_ptr` is not needed anymore
- Added new member functions:
  - `request` (const ref)
  - `request_ptr` (both const and non-const ptr)
  - `wait` (carries impl)
  - `test` (carries impl)
  - Related friend functions call these directly
- Renamed callback registration:
  - ~~`get_inner`~~ -> `request`
  - ~~`call_after_wait`~~ -> `add_callback`
- Reworked `wait_all`:
  - Active-wait loop that polls the requests until all are completed
  - Same approach as used in `wait_any`

src/KokkosComm/nccl/recv.hpp
@@ -11,33 +11,32 @@
 #include <KokkosComm/datatype.hpp>
 #include "nccl_space.hpp"
 #include "handle.hpp"
-#include "req.hpp"
+#include "request.hpp"
 
-#include <KokkosComm/impl/contiguous.hpp>
 #include "impl/pack_traits.hpp"
-#include "impl/nccl_check.hpp"
+#include "impl/error_handling.hpp"
 
 namespace KokkosComm {
 namespace Experimental::nccl {
 
-namespace KC = KokkosComm;
-
 template <KokkosExecutionSpace ExecSpace, KokkosView RecvView>
-auto recv(const ExecSpace &space, RecvView &rv, int peer, ncclComm_t comm) -> Req<NcclSpace> {
+auto recv(const ExecSpace& space, RecvView& rv, int peer, ncclComm_t comm) -> Request<NcclSpace> {
   using T = typename RecvView::non_const_value_type;
   Kokkos::Tools::pushRegion("KokkosComm::Impl::recv");
 
-  Req<NcclSpace> req{space.cuda_stream()};
-  if (KC::is_contiguous(rv)) {
-    KC_NCCL_CHECK(
-        ncclRecv(KC::data_handle(rv), KC::span(rv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
+  Request<NcclSpace> req;
+  if (is_contiguous(rv)) {
+    KC_NCCL_CHECK(ncclRecv(data_handle(rv), span(rv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
   } else {
     using Packer = typename Impl::PackTraits<RecvView>::packer_type;
-    auto pckd_rv = KC::Impl::allocate_contiguous_for(space, "KC::nccl::recv pckd_rv", rv);
-    KC_NCCL_CHECK(ncclRecv(KC::data_handle(pckd_rv), KC::span(pckd_rv), datatype<NcclSpace, T>(), peer, comm,
-                           space.cuda_stream()));
-    Packer::unpack_into(space, rv, pckd_rv);
-    req.extend_view_lifetime(pckd_rv);
+    auto pckd_rv = Packer::allocate_packed_for(space, "pckd_rv", rv);
+    KC_NCCL_CHECK(
+        ncclRecv(data_handle(pckd_rv.view_), pckd_rv.count_, pckd_rv.datatype_, peer, comm, space.cuda_stream()));
+    req.capture_stream_state(space.cuda_stream());
+    req.add_callback([space, rv, pckd_rv]() {
+      Packer::unpack_into(space, rv, pckd_rv.view_);
+      space.fence("fence `pckd_rv` unpacking after NCCL call");
+    });
   }
   req.extend_view_lifetime(rv);
 
@@ -50,8 +49,8 @@ namespace Impl {
 
 template <KokkosView RecvView>
 struct Recv<RecvView, Kokkos::Cuda, Experimental::NcclSpace> {
-  static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace> &h, RecvView sv, int peer)
-      -> Req<Experimental::NcclSpace> {
+  static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace>& h, RecvView sv, int peer)
+      -> Request<Experimental::NcclSpace> {
     return Experimental::nccl::recv(h.space(), sv, peer, h.comm());
   }
 };

src/KokkosComm/nccl/send.hpp
@@ -11,31 +11,29 @@
 #include <KokkosComm/datatype.hpp>
 #include "nccl_space.hpp"
 #include "handle.hpp"
-#include "req.hpp"
+#include "request.hpp"
 
 #include "impl/pack_traits.hpp"
-#include "impl/nccl_check.hpp"
+#include "impl/error_handling.hpp"
 
 namespace KokkosComm {
 namespace Experimental::nccl {
 
-namespace KC = KokkosComm;
-
 template <KokkosExecutionSpace ExecSpace, KokkosView SendView>
-auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm) -> Req<NcclSpace> {
+auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm) -> Request<NcclSpace> {
   using T = typename SendView::non_const_value_type;
   Kokkos::Tools::pushRegion("KokkosComm::Impl::send");
 
-  Req<NcclSpace> req{space.cuda_stream()};
-  if (KC::is_contiguous(sv)) {
-    KC_NCCL_CHECK(
-        ncclSend(KC::data_handle(sv), KC::span(sv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
+  Request<NcclSpace> req;
+  if (is_contiguous(sv)) {
+    KC_NCCL_CHECK(ncclSend(data_handle(sv), span(sv), datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
   } else {
     using Packer = typename Impl::PackTraits<SendView>::packer_type;
-    auto args    = Packer::pack(space, sv);
+    auto pckd_sv = Packer::pack(space, "pckd_sv", sv);
     KC_NCCL_CHECK(
-        ncclSend(KC::data_handle(args.view_), args.count_, datatype<NcclSpace, T>(), peer, comm, space.cuda_stream()));
-    req.extend_view_lifetime(args.view_);
+        ncclSend(data_handle(pckd_sv.view_), pckd_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream()));
+    req.capture_stream_state(space.cuda_stream());
+    req.extend_view_lifetime(pckd_sv.view_);
   }
   req.extend_view_lifetime(sv);
 
@@ -49,7 +47,7 @@ namespace Impl {
 template <KokkosView SendView>
 struct Send<SendView, Kokkos::Cuda, Experimental::NcclSpace> {
   static auto execute(Handle<Kokkos::Cuda, Experimental::NcclSpace>& h, SendView sv, int peer)
-      -> Req<Experimental::NcclSpace> {
+      -> Request<Experimental::NcclSpace> {
     return Experimental::nccl::send(h.space(), sv, peer, h.comm());
   }
 };

perf_tests/mpi/test_2d_halo.cpp
@@ -10,8 +10,8 @@
 void noop(benchmark::State, MPI_Comm) {}
 
 template <typename Space, typename View>
-void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, int ny, int rx, int ry, int rs,
-               const View &v) {
+void send_recv(benchmark::State&, MPI_Comm comm, const Space& space, int nx, int ny, int rx, int ry, int rs,
+               const View& v) {
   KokkosComm::Handle<> h{space, comm};
 
   // 2D index of nbrs in minus and plus direction (periodic)
@@ -35,7 +35,7 @@ void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, in
   auto ym1_s = Kokkos::subview(v, make_pair(1, nx + 1), 1, Kokkos::ALL);
   auto ym1_r = Kokkos::subview(v, make_pair(1, nx + 1), 0, Kokkos::ALL);
 
-  std::vector<KokkosComm::Req<>> reqs;
+  std::vector<KokkosComm::Request<>> reqs;
   // std::cerr << get_rank(rx, ry) << " -> " << get_rank(xp1, ry) << "\n";
   reqs.push_back(KokkosComm::send(h, xp1_s, get_rank(xp1, ry)));
   reqs.push_back(KokkosComm::send(h, xm1_s, get_rank(xm1, ry)));
@@ -51,9 +51,9 @@ void send_recv(benchmark::State &, MPI_Comm comm, const Space &space, int nx, in
   KokkosComm::wait_all(reqs);
 }
 
-void benchmark_2dhalo(benchmark::State &state) {
+void benchmark_2dhalo(benchmark::State& state) {
   using Scalar    = double;
-  using grid_type = Kokkos::View<Scalar ***, Kokkos::LayoutRight>;
+  using grid_type = Kokkos::View<Scalar***, Kokkos::LayoutRight>;
 
   // problem size per rank
   int nx     = 512;

src/KokkosComm/KokkosComm.hpp
@@ -15,7 +15,7 @@
 #include "mpi/channel.hpp"
 #include "mpi/comm_mode.hpp"
 #include "mpi/handle.hpp"
-#include "mpi/req.hpp"
+#include "mpi/request.hpp"
 
 #include "mpi/irecv.hpp"
 #include "mpi/isend.hpp"
@@ -36,7 +36,7 @@
 #include "nccl/nccl_space.hpp"
 
 #include "nccl/handle.hpp"
-#include "nccl/req.hpp"
+#include "nccl/request.hpp"
 
 #include "nccl/recv.hpp"
 #include "nccl/send.hpp"

src/KokkosComm/collective.hpp
@@ -8,6 +8,9 @@
 #include "fwd.hpp"
 #include "reduction_op.hpp"
 #if defined(KOKKOSCOMM_ENABLE_MPI)
+#include "mpi/mpi_space.hpp"
+#include "mpi/handle.hpp"
+#include "mpi/request.hpp"
 #include "mpi/broadcast.hpp"
 #include "mpi/allgather.hpp"
 #include "mpi/alltoall.hpp"
@@ -15,9 +18,12 @@
 #include "mpi/reduce.hpp"
 #endif
 #if defined(KOKKOSCOMM_ENABLE_NCCL)
+#include "nccl/nccl_space.hpp"
+#include "nccl/handle.hpp"
+#include "nccl/request.hpp"
 #include "nccl/broadcast.hpp"
-#include "nccl/alltoall.hpp"
 #include "nccl/allgather.hpp"
+#include "nccl/alltoall.hpp"
 #include "nccl/allreduce.hpp"
 #include "nccl/reduce.hpp"
 #endif
@@ -27,7 +33,7 @@ namespace KokkosComm::Experimental {
 /// Copy the `v` view from the `root` rank to all ranks' `v` view.
 template <KokkosView View, KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace = DefaultCommunicationSpace>
-auto broadcast(Handle<ExecSpace, CommSpace>& h, View v, int root) -> Req<CommSpace> {
+auto broadcast(Handle<ExecSpace, CommSpace>& h, View v, int root) -> Request<CommSpace> {
   return Impl::Broadcast<View, ExecSpace, CommSpace>::execute(h, v, root);
 }
 
@@ -37,7 +43,7 @@ auto broadcast(Handle<ExecSpace, CommSpace>& h, View v, int root) -> Req<CommSpa
 /// Note: this assumes the span of the `rv` view to be `h.size() * KokkosComm::span(sv)`.
 template <KokkosView SendView, KokkosView RecvView, KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace = DefaultCommunicationSpace>
-auto allgather(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv) -> Req<CommSpace> {
+auto allgather(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv) -> Request<CommSpace> {
   return Impl::AllGather<SendView, RecvView, ExecSpace, CommSpace>::execute(h, sv, rv);
 }
 
@@ -49,7 +55,7 @@ auto allgather(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv) 
 /// Note: this assumes the span of both `sv` and `rv` views to be `h.size * count`.
 template <KokkosView SendView, KokkosView RecvView, KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace = DefaultCommunicationSpace>
-auto alltoall(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, int count) -> Req<CommSpace> {
+auto alltoall(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, int count) -> Request<CommSpace> {
   return Impl::AllToAll<SendView, RecvView, ExecSpace, CommSpace>::execute(h, sv, rv, count);
 }
 
@@ -57,7 +63,7 @@ auto alltoall(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, i
 template <KokkosView SendView, KokkosView RecvView, ReductionOperator RedOp,
           KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace   = DefaultCommunicationSpace>
-auto allreduce(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, RedOp) -> Req<CommSpace> {
+auto allreduce(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, RedOp) -> Request<CommSpace> {
   return Impl::AllReduce<SendView, RecvView, RedOp, ExecSpace, CommSpace>::execute(h, sv, rv);
 }
 
@@ -67,7 +73,7 @@ auto allreduce(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, 
 template <KokkosView SendView, KokkosView RecvView, ReductionOperator RedOp,
           KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace   = DefaultCommunicationSpace>
-auto reduce(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, int root, RedOp) -> Req<CommSpace> {
+auto reduce(Handle<ExecSpace, CommSpace>& h, const SendView sv, RecvView rv, int root, RedOp) -> Request<CommSpace> {
   return Impl::Reduce<SendView, RecvView, RedOp, ExecSpace, CommSpace>::execute(h, sv, rv, root);
 }
 

src/KokkosComm/fwd.hpp
@@ -27,8 +27,9 @@ using FallbackCommunicationSpace = MpiSpace;
 #error at least one communication space must be enabled
 #endif
 
+/// @brief Template class for request wrappers.
 template <CommunicationSpace CommSpace = DefaultCommunicationSpace>
-class Req;
+class Request;
 
 template <KokkosExecutionSpace ExecSpace = Kokkos::DefaultExecutionSpace,
           CommunicationSpace CommSpace   = DefaultCommunicationSpace>

src/KokkosComm/impl/contiguous.hpp
@@ -14,7 +14,8 @@ namespace KokkosComm::Impl {
 
 template <KokkosView View>
 struct contiguous_view {
-  using type = Kokkos::View<typename View::non_const_data_type, Kokkos::LayoutRight, typename View::memory_space>;
+  using type = Kokkos::View<typename View::non_const_data_type, typename View::execution_space::array_layout,
+                            typename View::memory_space>;
 };
 
 template <KokkosView View>

src/KokkosComm/mpi/CMakeLists.txt
@@ -8,7 +8,7 @@ target_sources(
         # Objects
         mpi_space.hpp
         handle.hpp
-        req.hpp
+        request.hpp
         comm_mode.hpp
         # Point-to-point
         send.hpp

commit 6e0396714c94a8bffef6e9fc2c84e8ad7ebc21dd

fix(nccl): capture stream state in send/recv for all code paths
Previously, `capture_stream_state` was only called in the non-contiguous
(packed) view path, causing the stream state to not be captured for
contiguous views in NCCL Send and Recv operations.

This fixes a bug introduced in #216 that made the SNL NCCL CI fail on
the P2P test.

src/KokkosComm/nccl/recv.hpp
@@ -33,12 +33,12 @@ auto recv(const ExecSpace& space, RecvView& rv, int peer, ncclComm_t comm) -> Re
     KC_NCCL_CHECK(
         ncclRecv(data_handle(pckd_rv.view_), pckd_rv.count_, pckd_rv.datatype_, peer, comm, space.cuda_stream())
     );
-    req.capture_stream_state(space.cuda_stream());
     req.add_callback([space, rv, pckd_rv]() {
       Packer::unpack_into(space, rv, pckd_rv.view_);
       space.fence("fence `pckd_rv` unpacking after NCCL call");
     });
   }
+  req.capture_stream_state(space.cuda_stream());
   req.extend_view_lifetime(rv);
 
   Kokkos::Tools::popRegion();

src/KokkosComm/nccl/send.hpp
@@ -33,9 +33,9 @@ auto send(const ExecSpace& space, const SendView& sv, int peer, ncclComm_t comm)
     KC_NCCL_CHECK(
         ncclSend(data_handle(pckd_sv.view_), pckd_sv.count_, pckd_sv.datatype_, peer, comm, space.cuda_stream())
     );
-    req.capture_stream_state(space.cuda_stream());
     req.extend_view_lifetime(pckd_sv.view_);
   }
+  req.capture_stream_state(space.cuda_stream());
   req.extend_view_lifetime(sv);
 
   Kokkos::Tools::popRegion();